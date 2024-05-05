506328975b8b342e4505682b36fc7cb5147b6160
Icaro Henrique <icaro.henrique001@gmail.com>
2024-05-05 16:28:12 -0300

    Finalizers tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 MARKER = """\
 integration: Mark integration tests
 unit: Mark unit tests
@@ -9,3 +10,10 @@ low: Low Prioriry
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line('markers', line)
+
+
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request):
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield
