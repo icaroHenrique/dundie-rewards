98566be38a36d36e97a6e311877e7d8b282ba59e
Icaro Henrique <icaro.henrique001@gmail.com>
2024-05-06 16:28:48 -0300

    merge cli.py

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,24 +1,25 @@
-import argparse
+import click
+import pkg_resources
+from dundie import core
 
-from dundie.core import load  # noqa
 
 
+@click.group()
+@click.version_option(pkg_resources.get_distribution("dundie").version)
 def main():
-    parse = argparse.ArgumentParser(
-        description="Dunder Mifflin Rewards CLI",
-        epilog="Enjoy and use with cautious",
-    )
-    parse.add_argument(
-        "subcommand",
-        type=str,
-        help="The subcommand to run",
-        choices=("load", "show", "send"),
-    )
-    parse.add_argument(
-        "filepath",
-        type=str,
-        help="File path to load",
-    )
-    args = parse.parse_args()
+    """Dunder Mifflin Rewards System.
+    
+    This cli application controls DM rewards
+    """
 
-    print(*globals()[args.subcommand](args.filepath))
+@main.command()
+@click.argument("filepath", type=click.Path(exists=True))
+def load(filepath):
+    """Loads the file to the database"""
+    result = core.load(filepath)
+    header = ["name", "dept", "role", "e-mail"]
+    for person in result:
+        print("-" * 30)
+        for key, value in zip(header, person.split(",")):
+            print(f"{key} -> {value.strip()}")
+    print("-" * 30)
